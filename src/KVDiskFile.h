120bfcb4fe40e4438fbd5e76a44b942ffd084e95
Giorgos Margaritis <gmargari@gmail.com>
2011-06-06 14:05:06 +0000

    Small changes (removed spaces, etc)

--- src/KVDiskFile.h
+++ src/KVDiskFile.h
@@ -9,14 +9,14 @@ class KVDiskFile {
 
 friend class KVDiskFileInputStream;
 friend class KVDiskFileOutputStream;
-    
+
 public:
 
     /**
      * constructor
      */
     KVDiskFile();
-    
+
     /**
      * destructor
      */
@@ -24,7 +24,7 @@ public:
 
     /**
      * open file for read and write (create if not existing)
-     * 
+     *
      * @return normally, should return true
      */
     bool open(char *filename);
@@ -32,43 +32,32 @@ public:
 
     /**
      * open an existing file
-     * 
+     *
      * @return true if file exists and was succesfully opened. otherwise false
      */
     bool open_existing(char *filename);
-    
+
     /**
      * open a new unique file in /tmp/ (see TMPFILEDIR, TMPFILEPREFIX in Global.h)
-     * 
+     *
      * @return normally, should return true
      */
     bool open_unique();
-    
+
     /**
-     * delete file from disk 
+     * delete file from disk
      */
     void delete_from_disk();
-    
-//     /**
-//      * file system name of this file
-//      */
+
+//     // file system name of this file
 //     char *name();    // NOTE: a VFile can be composed of several disk files. modify function to return array of names?!
-// 
-//     /**
-//      * rewind file
-//      */
+//     // rewind file
 //     void rewind();
-//
-//     /**
-//      * flush all pending writes to disk
-//      */
+//     // flush all pending writes to disk
 //     void sync();
-//
-//     /**
-//      * close file
-//      */
+//     // close file
 //     void close();
-    
+
 protected:
 
     void sanity_check();
