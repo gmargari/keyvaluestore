b3fc141597765e29e99e1cbe3ca6acceb060ff1b
Giorgos Margaritis <gmargari@gmail.com>
2011-06-13 22:58:20 +0000

    Unimportant checkin, removed useless defines from Global.h...

--- src/Global.h
+++ src/Global.h
@@ -11,8 +11,6 @@
 // default values
 #define DEFAULT_MEMSTORE_SIZE                       1048576 // 1MB
 #define MAX_INDEX_DIST                                  100 // 100B
-#define READBUFSIZE                ((size_t)(64*1024*1024))
-#define MERGEBUFSIZE               ((size_t)(64*1024*1024))
 #define MAX_KVTSIZE          ((unsigned long)(1*1024*1024)) // 1MB
 #define SCANNERBUFSIZE       ((unsigned long)(4*1024*1024)) // 4MB (must be at least 2*MAX_KVTSIZE + 2*sizeof(uint64_t))
 
