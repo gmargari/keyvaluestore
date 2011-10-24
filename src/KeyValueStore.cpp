50f1b8bd25d1f4c55355a315c72f5d35e371e58e
Giorgos Margaritis <gmargari@gmail.com>
2011-10-24 18:33:03 +0000

    Corrected previous check in (had accidentally put part of code in comments).

--- src/KeyValueStore.cpp
+++ src/KeyValueStore.cpp
@@ -103,32 +103,32 @@ bool KeyValueStore::put(const char *key, const char *value)
     return m_memstore->put(key, value);
 }
 
-// /*============================================================================
-//  *                                   get
-//  *============================================================================*/
-// bool KeyValueStore::get(const char *key, char **value, uint64_t *timestamp)
-// {
-//     // if key found in memstore, return since this is the most recent value:
-//     if (m_memstore->get(key, value, timestamp)) {
-//         return true;
-//     }
-//     // else, search in diskstore:
-//     else {
-//         return m_diskstore->get(key, value, timestamp);
-//     }
-// }
-//
-// /*============================================================================
-//  *                                   get
-//  *============================================================================*/
-// bool KeyValueStore::get(const char *key, uint64_t timestamp, char **value)
-// {
-//     if (m_memstore->get(key, timestamp, value)) {
-//         return true;
-//     } else {
-//         return m_diskstore->get(key, timestamp, value);
-//     }
-// }
+/*============================================================================
+ *                                   get
+ *============================================================================*/
+bool KeyValueStore::get(const char *key, char **value, uint64_t *timestamp)
+{
+    // if key found in memstore, return since this is the most recent value:
+    if (m_memstore->get(key, value, timestamp)) {
+        return true;
+    }
+    // else, search in diskstore:
+    else {
+        return m_diskstore->get(key, value, timestamp);
+    }
+}
+
+/*============================================================================
+ *                                   get
+ *============================================================================*/
+bool KeyValueStore::get(const char *key, uint64_t timestamp, char **value)
+{
+    if (m_memstore->get(key, timestamp, value)) {
+        return true;
+    } else {
+        return m_diskstore->get(key, timestamp, value);
+    }
+}
 
 /*============================================================================
  *                             get_num_mem_keys
