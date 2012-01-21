0f60536b76fe601b0e28522a4bd95b3b70c0415d
Giorgos Margaritis <gmargari@gmail.com>
2012-01-21 16:14:46 +0200

    Replaced some #defines with constants in Global.h

--- src/Global.h
+++ src/Global.h
@@ -3,6 +3,8 @@
 #ifndef SRC_GLOBAL_H_
 #define SRC_GLOBAL_H_
 
+#include <stdint.h>
+
 // set debug level:
 //  0 - debugging disabled
 //  1 - normal mode     (assertions and sanity_check()s enabled)
@@ -13,21 +15,21 @@
 //==============================================================================
 // default values
 //==============================================================================
-#define DEFAULT_MEMSTORE_SIZE        104857600LL  // total memory used by prog
-#define MAX_FILE_SIZE               2147483647LL  // max size of disk files
-#define MAX_KVTSIZE    ((uint64_t)(1*1024*1024))  // max size for a key or value
-#define MAX_INDEX_DIST                     65536
-#define MERGE_BUFSIZE  ((uint64_t)(4*1024*1024))
+const uint64_t DEFAULT_MEMSTORE_SIZE = 104857600;  // total memory used by prog
+const uint64_t MAX_FILE_SIZE = 2147483647;         // max size of disk files
+const uint32_t MAX_KVTSIZE = 1048576;              // max size for key or value
+const uint32_t MAX_INDEX_DIST = 65536;             // for VFileIndex
+const uint64_t MERGE_BUFSIZE = 4194304;            // size of IOs done for merge
 // (MERGE_BUFSIZE must be at least 2*MAX_KVTSIZE + 2*sizeof(uint64_t))
 
 //==============================================================================
 // compaction managers' default values
 //==============================================================================
-#define DEFAULT_GEOM_R                         3
-#define DEFAULT_GEOM_P                         0
-#define DEFAULT_RNG_BLOCKSIZE        268435456LL
-#define DEFAULT_RNG_FLUSHMEMSIZE             0LL  // 0: flush biggest range only
-#define DEFAULT_CASS_K                         4
+const int      DEFAULT_GEOM_R = 3;
+const int      DEFAULT_GEOM_P = 0;
+const uint64_t DEFAULT_RNG_BLOCKSIZE = 268435456;
+const uint64_t DEFAULT_RNG_FLUSHMEMSIZE = 0;      // 0: flush biggest range only
+const int      DEFAULT_CASS_K = 4;
 
 //==============================================================================
 // vfile-related defines
