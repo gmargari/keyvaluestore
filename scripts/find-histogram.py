c26e2878337d119511743205f12f8cd8cda5b7c1
Giorgos Margaritis <gmargari@gmail.com>
2012-02-08 15:23:00 +0200

    find_histogram.py now reads data from stdin

--- scripts/find-histogram.py
+++ scripts/find-histogram.py
@@ -2,30 +2,33 @@
 
 import sys
 
-if (len(sys.argv) != 3):
-	print "syntax: %s <filename> <bucketsize>" % (sys.argv[0])
-	sys.exit()
+if (len(sys.argv) != 2):
+    print "syntax: %s <bucketsize>" % (sys.argv[0])
+    sys.exit()
 
 
-lines = open(sys.argv[1]).read().split("\n")
-bsize = int(sys.argv[2]) # bucket size
+lines = sys.stdin.read().split("\n")
+bsize = int(sys.argv[1]) # bucket size
 
 # max bucket value (= num of buckets)
 mmax = 0
 for i in range(0, len(lines)):
-	if (len(lines[i]) > 0):
-		m = int(lines[i]) / bsize
-		mmax = max(mmax,m)
+    if (len(lines[i]) > 0):
+        m = int(lines[i]) / bsize
+        mmax = max(mmax,m)
 
 # count terms per bucket
 a = [0]*(mmax+1)
 for i in range(0, len(lines)):
-	if (len(lines[i]) > 1):
-		m = int(lines[i]) / bsize
-		a[m] += 1
+    if (len(lines[i]) > 1):
+        m = int(lines[i]) / bsize
+        a[m] += 1
 
 
 # print buckets
-for i in range(0, len(a)):
-	if (a[i] > 0):
-		print "%d %d" % (i*bsize, a[i])
+try:
+    for i in range(0, len(a)):
+        if (a[i] > 0):
+            print "%d %d" % (i*bsize, a[i])
+except IOError, e:
+    pass # do nothing
